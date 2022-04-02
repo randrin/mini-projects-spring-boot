0230e6dfae7807a8f9929fa0b8cab838794fce23
Randrin Nzeukang <nrandrin@eservicios.indracompany.com>
2022-04-02 05:17:43 +0200

    First commit

---
+++ sending-email-attachment/src/main/java/com/randrin/sending/email/attachment/service/SendEmailService.java
@@ -0,0 +1,52 @@
+package com.randrin.sending.email.attachment.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+@Service
+public class SendEmailService {
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public String sendSimpleEmail(String toEmail, String body, String subject) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        javaMailSender.send(message);
+
+        return "Simple message sending ...";
+    }
+
+    public void sendEmailWithAttachment(String toEmail, String body, String subject, String attachment) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+        mimeMessageHelper.setFrom(fromEmail);
+        mimeMessageHelper.setTo(toEmail);
+        mimeMessageHelper.setText(body);
+        mimeMessageHelper.setSubject(subject);
+
+        FileSystemResource fileSystemResource = new FileSystemResource(new File(attachment));
+        mimeMessageHelper.addAttachment(fileSystemResource.getFilename(), fileSystemResource);
+
+        javaMailSender.send(mimeMessage);
+
+        System.out.println("Attachment message sending ...");
+    }
+}
---
+++ sending-email-attachment/src/test/java/com/randrin/sending/email/attachment/SendingEmailAttachmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randrin.sending.email.attachment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendingEmailAttachmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
